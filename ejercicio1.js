07397f87da36382fdf23cad4ea0310c55b4f3497
brianbayona <brianmauriciobayonaravelo@gmail.com>
2026-02-27 17:32:48 -0500

    agrego puntos 1,2 y 3 en rama develop con style bem incluido

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+/* C. Actividades de apropiación del conocimiento. */
+/** 1. Saludo personalizado */
+/**
+ * Declaramos las variables
+ */
+let nombre, edad, mensaje;
+
+/**
+ * Datos de ingreso
+ */
+nombre = prompt("Ingrese su nombre:");
+edad = prompt("Ingrese su edad:");
+
+/**
+ * Operaciones
+ */
+mensaje = "Hola " + nombre + ", tienes " + edad + " años.";
+
+/**
+ * Datos de salida
+ */
+console.log(mensaje);
