9ddc52c24dcc82f6c056cf00b1b375b0de1bc0f0
andrejoser39-dot <andrejoser39@gmail.com>
2026-02-27 17:29:26 -0500

    cree el punto 6,7

---
+++ app2.js
@@ -0,0 +1,12 @@
+//punto 7//
+// 1. Solicitar el número
+let numero = prompt("Introduce un número:");
+numero = Number(numero); // Convertir a número
+
+// 2. Calcular valores
+let doble = numero * 2;
+let triple = numero * 3;
+
+// 3. Mostrar resultados
+console.log("El doble es: " + doble);
+console.log("El triple es: " + triple);
